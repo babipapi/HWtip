6369472c21f1d0944ff519ae748604516add3da7
Daniel <babipapi@gmail.com>
2019-04-25 19:34:05 +0800

    fix1

--- HWtip/ViewController.swift
+++ HWtip/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var personTextField: UITextField!
+    @IBOutlet weak var priceTextField: UITextField!
+    @IBOutlet weak var tipTextField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var avgLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculate(_ sender: UIButton) {
+        let priceText = priceTextField.text!
+        let tipText = tipTextField.text!
+        let personText = personTextField.text!
+        let price = Int(priceText)
+        let tip = Int(tipText)
+        let person = Int(personText)
+        let total = price! + (price! * tip! / 100)
+        if price != nil,tip != nil,person != nil {
+            let avg = total / person!
+            avgLabel.text = String(avg)
+        } else if price != nil,tip != nil{
+            totalLabel.text = String(total)
+            
+        }
+        view.endEditing(true)
+    }
+    @IBAction func restart(_ sender: UIButton) {
+        priceTextField.text = nil
+        tipTextField.text = nil
+        personTextField.text = nil
+        totalLabel.text = nil
+        avgLabel.text = nil
+    }
+    
 }
 
